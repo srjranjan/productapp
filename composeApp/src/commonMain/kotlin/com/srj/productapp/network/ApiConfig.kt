716ccf5cb787757734901684950b3c0970949f44
Sudhanshu Ranjan <sudhanshu.ranjan@nobroker.in>
2025-06-09 06:26:19 +0530

    refactor

--- composeApp/src/commonMain/kotlin/com/srj/productapp/network/ApiConfig.kt
+++
@@ -1,7 +0,0 @@
-package com.srj.productapp.network
-
-object ApiConfig {
-    const val BASE_URL = "https://mocki.io/v1/"
-    const val PRODUCTS_ENDPOINT = "your-products-endpoint"
-    const val PROFILE_ENDPOINT = "your-profile-endpoint"
-}
--- composeApp/src/commonMain/kotlin/com/srj/productapp/network/MainRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.srj.productapp.network
-
-import com.srj.productapp.data.model.Product
-import com.srj.productapp.data.model.UserProfile
-
-interface MainRepository {
-    suspend fun getProducts(): List<Product>
-    suspend fun getProfile(): UserProfile
-}
